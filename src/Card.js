82f59ed2c25ff0c558843699cb75062fd2869df7
Przemyslaw Tarkowski-EXT <przemyslaw.tarkowski@jeppesen.com>
2020-01-24 10:05:47 +0100

    add font and some features

--- src/Card.js
+++ src/Card.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Card = ({ name, email, id}) => {   //quicker version of (props) and then const {name, email,id} = props;
     return (
         <div className='bg-light-green dib br3 pa3 ma2 grow shadow-5'>
-            <img alt='robots' src={`https://robohash.org/${id}?set=set2`}/>
+            <img alt='robots' src={`https://robohash.org/${name}?set=set5`}/>
             <div>
                 <h2>{name}</h2>
                 <p>{email}</p>
--- src/robots.js
+++ src/robots.js
@@ -2,20 +2,38 @@ export const robots = [
     {
         id:1,
         name: 'dupa',
-        username: 'kupa',
+        username: 'dupa',
         email: 'dupa.kupa@wp.pl',
     },
     {
         id:2,
-        name: 'dupa2',
-        username: 'kupa2',
+        name: 'marianek',
+        username: 'marianek',
         email: 'dupa.kupa2@wp.pl',
     },
     {
         id:3,
-        name: 'dupa3',
-        username: 'kupa3',
+        name: 'zyguś',
+        username: 'zyguś',
         email: 'dupa.kupa3@wp.pl',
+    },
+    {
+        id:4,
+        name: 'wiesio',
+        username: 'wiesio',
+        email: 'dupa.kupa4@wp.pl',
+    },
+    {
+        id:5,
+        name: 'filutek',
+        username: 'filutek',
+        email: 'dupa.kupa5@wp.pl',
+    },
+    {
+        id:6,
+        name: 'konus',
+        username: 'konus',
+        email: 'dupa.kupa6@wp.pl',
     }
 
 ]
