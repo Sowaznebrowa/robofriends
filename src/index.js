3e5e3caadbcf90ffc0606be64ca84877bad6f3e3
Przemyslaw Tarkowski-EXT <przemyslaw.tarkowski@jeppesen.com>
2020-01-24 18:33:12 +0100

    add redux with async action

--- src/index.js
+++ src/index.js
@@ -4,8 +4,21 @@ import './index.css';
 import App from './containers/App'
 import * as serviceWorker from './serviceWorker';
 import 'tachyons';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware, combineReducers } from 'redux';
+import { createLogger } from 'redux-logger';
+import { searchRobots, requestRobots } from './reducers';
+import thunkMiddleware from 'redux-thunk';
 
-ReactDOM.render(<App/>,
+const logger = createLogger();
+
+const rootReducer = combineReducers({searchRobots, requestRobots});
+const store = createStore(rootReducer, applyMiddleware(thunkMiddleware, logger));
+
+ReactDOM.render(
+               <Provider store={store}>
+                    <App/>
+               </Provider>,
      document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
