752db749e18a1867f35574d8cb43c99fedf0d971
Przemyslaw Tarkowski-EXT <przemyslaw.tarkowski@jeppesen.com>
2020-01-24 10:18:58 +0100

    refactor project structure

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,8 +1,8 @@
 import React, {Component} from 'react';
-import CardList from "./CardList";
-import SearchBox from './SearchBox';
+import CardList from "../components/CardList";
+import SearchBox from '../components/SearchBox';
 import './App.css';
-import Scroll from './Scroll';
+import Scroll from '../components/Scroll';
 
 class App extends Component {
     constructor(){
@@ -26,8 +26,9 @@ class App extends Component {
     }
 
     render(){
-        const filteredRobots = this.state.robots.filter(robot =>{
-            return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase())
+        const {robots, searchfield} = this.state;
+        const filteredRobots = robots.filter(robot =>{
+            return robot.name.toLowerCase().includes(searchfield.toLowerCase())
         })
 
         return (
